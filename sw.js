066f95b37bdde79de1bb62a75c9ded8d267a9df2
scriptandme <cachulaa@gmail.com>
2020-07-28 01:04:29 -0400

    change swurl dinamic to local an production server

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE      = 'static_v3';
+const STATIC_CACHE      = 'static_v4';
 const DINAMIC_CACHE     = 'dinamic_v1';
 const INMUTABLE_CACHE   = 'inmutable_v1';
 
